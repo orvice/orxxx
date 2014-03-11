2d117c045cd5ab70b300616ee8067c9454e9cc98
orvice <orvice@gmail.com>
2014-03-11 23:59:19 +0800

    update

--- index.php
+++ index.php
@@ -2,12 +2,17 @@
 <?php
 get_header(); ?>
 
-<div id="content">
-    <?php if(have_posts()) : ?><?php while(have_posts()) : the_post(); ?>
-    <?php endwhile; ?>
-    <?php endif; ?>
+<div id="main">
+    <div id="content">
+       <?php if(have_posts()) : ?><?php while(have_posts()) : the_post(); ?>
+       <?php endwhile; ?>
+       <?php endif; ?>
+    </div>
+    <div id="sidebar">
+        <?php get_sidebar(); ?>
+    </div>
 </div>
 
 
-<?php get_sidebar(); ?>
+
 <?php get_footer(); ?>
